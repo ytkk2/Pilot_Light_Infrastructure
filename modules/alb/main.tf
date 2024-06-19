8a4bd7a2ef615e757815b991d1db2c5d1bd71387
ytkk2 <yutakakatojp@gmail.com>
2024-06-19 15:02:00 +0900

    Add health_check section on alb resource

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -12,6 +12,15 @@ resource "aws_lb_target_group" "tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-299"
+  }
 }
 
 resource "aws_lb_listener" "front_end" {
