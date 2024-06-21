219d4d2138903ec793c431d5089e89abfc55bb47
ytkk2 <yutakakatojp@gmail.com>
2024-06-21 10:03:17 +0900

    Add output named s3_bucket_name

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "mariadb_endpoint" {
   value       = module.rds.mariadb_endpoint
   description = "The endpoint of the MariaDB instance"
 }
+
+output "s3_bucket_name" {
+  value = module.cloudtrail.bucket_name
+  description = "Name of the s3 bucket for cloudtrail"
+}
